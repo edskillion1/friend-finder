8be49d114d2aa36cb1087f619dfdc9d122a0d761
Ed Skillion <edskillion@outlook.com>
2018-11-19 16:44:51 -0800

    Friend Finder from Ed Skillion

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # friend-finder
-Friend Finder application
+Friend Finder application - Node and Express Servers
+
+This full stack site is used to take a survey and match you with a friend that has similar attributes based on the input. The results will display with the name and picture of the person you are most likely to have a good friendship with.
+
+Technologies used
+-Node.js
+-express NPM package
+-path NPM package
+
---
+++ app/data/friend.js
@@ -0,0 +1,102 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+ {
+  name: 'Ed_Skillion',
+    profilePic: 'http://www.scentedweb.com/codeclass/friendpics/Ed_Skillion.jpg',
+    scores: [
+      "4",
+      "3",
+      "2",
+      "4",
+      "4",
+      "3",
+      "4",
+      "4",
+      "4",
+      "4"
+    ]
+  },
+  {
+    name: 'Antonio Gates',
+    profilePic: 'http://www.scentedweb.com/codeclass/friendpics/Antonio_Gates.jpg',
+    scores: [
+      "4",
+      "4",
+      "3",
+      "5",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4"
+    ]
+  },
+  {
+	  name: 'Rachael Ray',
+    profilePic: 'http://www.scentedweb.com/codeclass/friendpics/Rachael_Ray.jpg',
+    scores: [
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4",
+      "5",
+      "4",
+      "4",
+      "4"
+    ]
+  },
+  {
+  name: 'Kawhi Leanord',
+    profilePic: 'http://www.scentedweb.com/codeclass/friendpics/Kawhi_Leanord.jpg',
+    scores: [
+      "5",
+      "3",
+      "2",
+      "4",
+      "5",
+      "4",
+      "5",
+      "4",
+      "5",
+      "4"
+    ]
+  },
+  {
+  name: 'Johnny Depp',
+    profilePic: 'http://www.scentedweb.com/codeclass/friendpics/Johnny_Depp.jpg',
+    scores: [
+      "5",
+      "3",
+      "5",
+      "3",
+      "4",
+      "4",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+    {
+  name: 'Fedora Linux',
+    profilePic: 'http://www.scentedweb.com/codeclass/friendpics/fedora-linux.jpg',
+    scores: [
+      "4",
+      "5",
+      "5",
+      "2",
+      "4",
+      "5",
+      "4",
+      "4",
+      "3",
+      "4"
+    ]
+  }
+]
+
+module.exports = friendList;
